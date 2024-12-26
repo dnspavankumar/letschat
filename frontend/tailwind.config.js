998a7a1aabfc8b95c53fa7ce71b7bc65d9b5a6c1
dnspavankumar <dnspavankumar722@gmail.com>
2024-12-26 15:52:16 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import daisyUI from 'daisyui';  // Use import instead of require
+
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyUI],  // Use the imported daisyUI plugin
 };
